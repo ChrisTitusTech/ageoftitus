eddbcc9d1c80d3285b72580ccc7411c9d442404f
Chris Titus <contact@christitus.com>
2024-10-16 09:58:47 -0500

    remove submodule

---
+++ themes/cyberscape/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["content/**/*.md", "layouts/**/*.html", "../../content/**/*.md"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
